c75b897e71600afbe8aeace99bbbabc127ba52b2
Sergey S. Betke <sergey.s.betke@yandex.ru>
2012-10-09 01:14:58 +0400

    * выделил в отдельный репозиторий мз ITG.WinAPI.

---
+++ ITG.WinAPI.User32.psm1
@@ -0,0 +1,41 @@
+﻿Add-Type @" 
+
+using System; 
+using System.IO; 
+using System.Runtime.InteropServices; 
+
+namespace ITG {
+
+public class WinAPI { 
+	
+// http://msdn.microsoft.com/en-us/library/windows/desktop/ms633545(v=vs.85).aspx
+[DllImport("user32.dll")] 
+[return: MarshalAs(UnmanagedType.Bool)] 
+public
+static
+extern
+bool
+SetWindowPos(
+	[In,  MarshalAs(UnmanagedType.U2)] IntPtr hWnd, 
+	[In,  MarshalAs(UnmanagedType.U2)] IntPtr hWndInsertAfter,
+	[In] int x,  [In] int y, 
+	[In] int cx, [In] int cy, 
+	[In,  MarshalAs(UnmanagedType.U2)] uint uFlags
+);
+
+// http://msdn.microsoft.com/en-us/library/windows/desktop/ms633539(v=vs.85).aspx
+[DllImport("user32.dll")] 
+[return: MarshalAs(UnmanagedType.Bool)] 
+public
+static
+extern
+bool
+SetForegroundWindow(
+	[In,  MarshalAs(UnmanagedType.U2)] IntPtr hWnd
+);
+	
+} 
+
+}
+
+"@;
